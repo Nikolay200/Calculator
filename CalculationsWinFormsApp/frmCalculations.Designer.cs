baa36c05cb2e202236b90416341404bdeb8d8f72
Nikolay200 <kolekdoma@mail.ru>
2021-06-01 09:04:20 +0300

    Создал калькулятор

---
+++ CalculationsWinFormsApp/frmCalculations.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace CalculationsWinFormsApp
+{
+    partial class frmCalculations
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculations));
+            this.lblFactorial1 = new System.Windows.Forms.Label();
+            this.lblFactorial2 = new System.Windows.Forms.Label();
+            this.lblAddTwo = new System.Windows.Forms.Label();
+            this.lblRunLoops = new System.Windows.Forms.Label();
+            this.lblTotalCalculations = new System.Windows.Forms.Label();
+            this.btnFactorial1 = new System.Windows.Forms.Button();
+            this.btnFactorial2 = new System.Windows.Forms.Button();
+            this.btnAddTwo = new System.Windows.Forms.Button();
+            this.btnRunLoops = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblFactorial1
+            // 
+            resources.ApplyResources(this.lblFactorial1, "lblFactorial1");
+            this.lblFactorial1.Name = "lblFactorial1";
+            // 
+            // lblFactorial2
+            // 
+            resources.ApplyResources(this.lblFactorial2, "lblFactorial2");
+            this.lblFactorial2.Name = "lblFactorial2";
+            // 
+            // lblAddTwo
+            // 
+            resources.ApplyResources(this.lblAddTwo, "lblAddTwo");
+            this.lblAddTwo.Name = "lblAddTwo";
+            // 
+            // lblRunLoops
+            // 
+            resources.ApplyResources(this.lblRunLoops, "lblRunLoops");
+            this.lblRunLoops.Name = "lblRunLoops";
+            // 
+            // lblTotalCalculations
+            // 
+            resources.ApplyResources(this.lblTotalCalculations, "lblTotalCalculations");
+            this.lblTotalCalculations.Name = "lblTotalCalculations";
+            // 
+            // btnFactorial1
+            // 
+            resources.ApplyResources(this.btnFactorial1, "btnFactorial1");
+            this.btnFactorial1.Name = "btnFactorial1";
+            this.btnFactorial1.UseVisualStyleBackColor = true;
+            this.btnFactorial1.Click += new System.EventHandler(this.btnFactorial1_Click);
+            // 
+            // btnFactorial2
+            // 
+            resources.ApplyResources(this.btnFactorial2, "btnFactorial2");
+            this.btnFactorial2.Name = "btnFactorial2";
+            this.btnFactorial2.UseVisualStyleBackColor = true;
+            this.btnFactorial2.Click += new System.EventHandler(this.btnFactorial2_Click);
+            // 
+            // btnAddTwo
+            // 
+            resources.ApplyResources(this.btnAddTwo, "btnAddTwo");
+            this.btnAddTwo.Name = "btnAddTwo";
+            this.btnAddTwo.UseVisualStyleBackColor = true;
+            this.btnAddTwo.Click += new System.EventHandler(this.btnAddTwo_Click);
+            // 
+            // btnRunLoops
+            // 
+            resources.ApplyResources(this.btnRunLoops, "btnRunLoops");
+            this.btnRunLoops.Name = "btnRunLoops";
+            this.btnRunLoops.UseVisualStyleBackColor = true;
+            this.btnRunLoops.Click += new System.EventHandler(this.btnRunLoops_Click);
+            // 
+            // txtValue
+            // 
+            resources.ApplyResources(this.txtValue, "txtValue");
+            this.txtValue.Name = "txtValue";
+            // 
+            // frmCalculations
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnRunLoops);
+            this.Controls.Add(this.btnAddTwo);
+            this.Controls.Add(this.btnFactorial2);
+            this.Controls.Add(this.btnFactorial1);
+            this.Controls.Add(this.lblTotalCalculations);
+            this.Controls.Add(this.lblRunLoops);
+            this.Controls.Add(this.lblAddTwo);
+            this.Controls.Add(this.lblFactorial2);
+            this.Controls.Add(this.lblFactorial1);
+            this.Name = "frmCalculations";
+            this.Load += new System.EventHandler(this.frmCalculations_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFactorial1;
+        private System.Windows.Forms.Label lblFactorial2;
+        private System.Windows.Forms.Label lblAddTwo;
+        private System.Windows.Forms.Label lblRunLoops;
+        private System.Windows.Forms.Label lblTotalCalculations;
+        private System.Windows.Forms.Button btnFactorial1;
+        private System.Windows.Forms.Button btnFactorial2;
+        private System.Windows.Forms.Button btnAddTwo;
+        private System.Windows.Forms.Button btnRunLoops;
+        private System.Windows.Forms.TextBox txtValue;
+    }
+}
+
---
+++ CalculationsWinFormsApp/frmCalculations.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculationsWinFormsApp
+{
+    public partial class frmCalculations : Form
+    {
+        Calculator Calculator1;
+        public delegate void FHandler(double Value, double Calculations);
+        public delegate void A2Handler(int Value, double Calculations);
+        public delegate void LDHandler(double Calculations, int Count);
+
+        public frmCalculations()
+        {
+            InitializeComponent();
+            // Creates a new instance of Calculator.
+            Calculator1 = new Calculator();
+            Calculator1.FactorialComplete += new
+            Calculator.FactorialCompleteHandler(this.FactorialHandler);
+            Calculator1.FactorialMinusOneComplete += new
+            Calculator.FactorialCompleteHandler(this.FactorialMinusHandler);
+            Calculator1.AddTwoComplete += new
+            Calculator.AddTwoCompleteHandler(this.AddTwoHandler);
+            Calculator1.LoopComplete += new
+            Calculator.LoopCompleteHandler(this.LoopDoneHandler);
+        }
+
+        private void frmCalculations_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFactorial1_Click(object sender, EventArgs e)
+        {
+            Calculator1.varFact1 = int.Parse(txtValue.Text);
+            // Disables the btnFactorial1 until this calculation is complete.
+            btnFactorial1.Enabled = false;
+            //Calculator1.Factorial();
+            Calculator1.ChooseThreads(1);
+        }
+
+        private void btnFactorial2_Click(object sender, EventArgs e)
+        {
+            Calculator1.varFact2 = int.Parse(txtValue.Text);
+            btnFactorial2.Enabled = false;
+            //Calculator1.FactorialMinusOne();
+            Calculator1.ChooseThreads(2);
+        }
+
+        private void btnAddTwo_Click(object sender, EventArgs e)
+        {
+            Calculator1.varAddTwo = int.Parse(txtValue.Text);
+            btnAddTwo.Enabled = false;
+            //Calculator1.AddTwo();
+            Calculator1.ChooseThreads(3);
+        }
+
+        private void btnRunLoops_Click(object sender, EventArgs e)
+        {
+            Calculator1.varLoopValue = int.Parse(txtValue.Text);
+            btnRunLoops.Enabled = false;
+            // Lets the user know that a loop is running
+            lblRunLoops.Text = "Looping";
+            //Calculator1.RunALoop();
+            Calculator1.ChooseThreads(4);
+        }
+
+        private void FactorialHandler(double Value, double Calculations)
+        // Displays the returned value in the appropriate label.
+        {
+            this.BeginInvoke(new FHandler(FactHandler), new Object[]
+      {Value, Calculations});
+        }
+
+        private void FactorialMinusHandler(double Value, double Calculations)
+        {
+            this.BeginInvoke(new FHandler(Fact1Handler), new Object[]
+       {Value, Calculations});
+        }
+
+        private void AddTwoHandler(int Value, double Calculations)
+        {
+            this.BeginInvoke(new A2Handler(Add2Handler), new Object[]
+        {Value, Calculations});
+        }
+
+        private void LoopDoneHandler(double Calculations, int Count)
+        {
+            this.BeginInvoke(new LDHandler(LDoneHandler), new Object[]
+       {Calculations, Count});
+        }
+
+        public void FactHandler(double Value, double Calculations)
+        {
+            lblFactorial1.Text = Value.ToString();
+            // Re-enables the button so it can be used again.
+            btnFactorial1.Enabled = true;
+            // Updates the label that displays the total calculations performed
+            lblTotalCalculations.Text = "TotalCalculations are " +
+               Calculations.ToString();
+        }
+        public void Fact1Handler(double Value, double Calculations)
+        {
+            lblFactorial2.Text = Value.ToString();
+            btnFactorial2.Enabled = true;
+            lblTotalCalculations.Text = "TotalCalculations are " +
+               Calculations.ToString();
+        }
+        public void Add2Handler(int Value, double Calculations)
+        {
+            lblAddTwo.Text = Value.ToString();
+            btnAddTwo.Enabled = true;
+            lblTotalCalculations.Text = "TotalCalculations are " +
+               Calculations.ToString();
+        }
+        public void LDoneHandler(double Calculations, int Count)
+        {
+            btnRunLoops.Enabled = true;
+            lblRunLoops.Text = Count.ToString();
+            lblTotalCalculations.Text = "TotalCalculations are " +
+               Calculations.ToString();
+        }
+
+    }
+}
